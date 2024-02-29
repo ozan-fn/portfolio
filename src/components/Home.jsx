362029cfdee421c759f8bbb8932398d78cac0b3b
23SA11A032 <23SA11A032@students.amikompurwokerto.co.id>
2024-02-29 20:32:52 +0700

    init

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ export default function Home() {
     return (
         <div className="flex h-full items-center justify-center px-4 md:px-0">
             <div className="relative flex h-fit flex-col rounded-l-sm">
-                <ReactTyped className="font-gagalin select-none text-4xl" strings={["AKHMAD FAUZAN"]} typeSpeed={100} />
+                <ReactTyped className="font-gagalin select-none text-4xl md:text-5xl lg:text-6xl" strings={["AKHMAD FAUZAN"]} typeSpeed={100} />
                 <p>Full Stack Engineer</p>
                 <motion.div animate={{ height: ["0%", "100%"] }} transition={{ duration: 3.5, type: "spring" }} className="absolute -left-6 h-full w-2 bg-gray-300" />
             </div>
