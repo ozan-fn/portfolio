6b5202013a4e1b8d13af2b67de515658cbb5e8cb
ozan-fn <23sa11a032@students.amikompurwokerto.ac.id>
2025-12-09 00:08:42 +0700

    add: new components and configurations

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
+import { ArrowLeft, Loader2 } from "lucide-react";
+
+export default function Login() {
+    const router = useRouter();
+    const [loading, setLoading] = useState(false);
+
+    const handleGoogleSignin = async () => {
+        setLoading(true);
+        await authClient.signIn.social(
+            {
+                provider: "google",
+            },
+            {
+                // onSuccess: () => {
+                //     const returnUrl = new URLSearchParams(window.location.search).get("returnUrl");
+                //     router.push(returnUrl || "/admin");
+                //     setLoading(false);
+                // },
+                onError: () => {
+                    setLoading(false);
+                },
+            },
+        );
+    };
+
+    const handleBack = () => {
+        router.push("/");
+    };
+
+    return (
+        <div className="min-h-screen max-w-7xl mx-auto flex items-center justify-center bg-background relative">
+            {/* Back button top left */}
+            <div className="absolute top-12 left-12">
+                <Button variant="ghost" size="sm" onClick={handleBack}>
+                    <ArrowLeft className="w-4 h-4" /> Back
+                </Button>
+            </div>
+            {/* Main content */}
+            <div className="text-center max-w-md mx-auto px-4">
+                <Button onClick={handleGoogleSignin} variant="outline" className="w-full" disabled={loading}>
+                    {loading && <Loader2 className="w-4 h-4 animate-spin" />}
+                    Continue with Google
+                </Button>
+            </div>
+        </div>
+    );
+}
---
+++ src/lib/prisma.ts
@@ -0,0 +1,12 @@
+import "dotenv/config";
+import { PrismaClient } from "@/generated/prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
+
+const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL });
+const prisma = new PrismaClient({ adapter });
+
+const globalForPrisma = global as unknown as { prisma: typeof prisma };
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export { prisma };
