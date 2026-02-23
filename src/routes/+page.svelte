c7e736024d5a5b1715edbefcaedf40de346e5ae3
ozan-fn <ozan6825@gmail.com>
2026-02-24 05:08:36 +0700

    .

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import ThemeToggle from "$lib/components/theme-toggle.svelte";
+    import { Button } from "$lib/components/ui/button/index.js";
+</script>
+
+<div class="border-b py-2">
+    <div class="mx-auto max-w-7xl w-full">
+        <div class="flex flex-row">
+            <Button>Click me</Button>
+
+            <div class="ml-auto">
+                <ThemeToggle />
+            </div>
+        </div>
+    </div>
+</div>
