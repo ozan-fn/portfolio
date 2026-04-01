a89a8f3e27845b76f83eae3f4a8fce6dc46a78b3
ozan-fn <me@ozan.my.id>
2026-04-01 23:16:45 +0700

    Add mode watcher and theme toggle

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
+<script>
+    import ModeToggle from '$lib/components/mode-toggle.svelte';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<ModeToggle />
