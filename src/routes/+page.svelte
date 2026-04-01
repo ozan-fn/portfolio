eb5dd6974d114e275f9cc28582ec1b419ebd27eb
ozan-fn <me@ozan.my.id>
2026-04-01 23:51:24 +0700

    Add UI components and update deps
    Add avatar, badge, card, and separator components
    Add dependencies bits-ui and @humanspeak/svelte-motion
    Also add @internationalized/date
    Update homepage with svelte-motion demo and timer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,35 @@
-<script>
-    import ModeToggle from '$lib/components/mode-toggle.svelte';
+<script lang="ts">
+    import { motion } from '@humanspeak/svelte-motion';
+    import { onDestroy, onMount } from 'svelte';
+
+    // Gunakan $state agar Svelte tahu variabel ini harus dipantau perubahannya
+    let b = $state(true);
+
+    // Gunakan tipe yang lebih bersih untuk timer di lingkungan Browser
+    let timer: number | undefined;
+
+    onMount(() => {
+        console.log('Timer dimulai...');
+        timer = window.setTimeout(() => {
+            b = false;
+            console.log('Variabel b sekarang false');
+        }, 2000);
+    });
+
+    onDestroy(() => {
+        if (timer) {
+            clearTimeout(timer);
+            console.log('Timer dibersihkan!');
+        }
+    });
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<ModeToggle />
+<div class="max-w-7xl mx-auto min-h-screen py-6 w-full flex flex-col items-center gap-4">
+    {#if b}
+        <motion.div layoutId="color" transition={{ type: 'spring' }} class="size-24 bg-red-500 shadow-lg"></motion.div>
+    {:else}
+        <motion.div layoutId="color" transition={{ type: 'spring' }} class="ml-50 size-24 bg-green-500 shadow-lg"></motion.div>
+    {/if}
+
+    <button onclick={() => (b = !b)}>Toggle</button>
+</div>
